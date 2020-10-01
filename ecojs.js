9405af070a371d9a900ad36d77091d80e7e5cb09
Laurent-Pechikoff <72021592+Laurent-Pechikoff@users.noreply.github.com>
2020-10-01 09:29:04 +0200

    Add files via upload
    dernière version avec lien java

--- ecojs.js
+++ ecojs.js
@@ -1,3 +1,4 @@
 ScrollReveal().reveal('.titre',{delay: 500, duration: 3000});
-ScrollReveal().reveal('.partie1',{distance: '60px', origin: 'top'});
-ScrollReveal().reveal('.partie2');
+ScrollReveal().reveal('.intro',{reset: true, delay: 500});
+ScrollReveal().reveal('.partie1',{reset: true, delay: 500});
+ScrollReveal().reveal('.partie2',{reset: true, delay: 500});
